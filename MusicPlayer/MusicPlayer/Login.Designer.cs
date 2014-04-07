39bdd403d82ae5224cb255f3f70ca372d7cbcdc5
Joseph Morrill <joseph.morrill@mymail.champlain.edu>
2014-04-07 19:35:18 -0400

    Added Login

---
+++ MusicPlayer/MusicPlayer/Login.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace MusicPlayer
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.UsernameField = new System.Windows.Forms.TextBox();
+            this.PasswordField = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.CreateAccountButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(12, 9);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.UsernameLabel.TabIndex = 0;
+            this.UsernameLabel.Text = "Username";
+            this.UsernameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(12, 37);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 1;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // UsernameField
+            // 
+            this.UsernameField.Location = new System.Drawing.Point(74, 9);
+            this.UsernameField.Name = "UsernameField";
+            this.UsernameField.Size = new System.Drawing.Size(124, 20);
+            this.UsernameField.TabIndex = 2;
+            // 
+            // PasswordField
+            // 
+            this.PasswordField.Location = new System.Drawing.Point(74, 37);
+            this.PasswordField.Name = "PasswordField";
+            this.PasswordField.PasswordChar = '*';
+            this.PasswordField.Size = new System.Drawing.Size(124, 20);
+            this.PasswordField.TabIndex = 3;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(15, 72);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // CreateAccountButton
+            // 
+            this.CreateAccountButton.Location = new System.Drawing.Point(123, 72);
+            this.CreateAccountButton.Name = "CreateAccountButton";
+            this.CreateAccountButton.Size = new System.Drawing.Size(75, 23);
+            this.CreateAccountButton.TabIndex = 5;
+            this.CreateAccountButton.Text = "Create Account";
+            this.CreateAccountButton.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.LoginButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 111);
+            this.Controls.Add(this.CreateAccountButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordField);
+            this.Controls.Add(this.UsernameField);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox UsernameField;
+        private System.Windows.Forms.TextBox PasswordField;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button CreateAccountButton;
+    }
+}
