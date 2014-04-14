94cb618921f2eda650a4c77cac6e65bfa5fd8b33
Spencer Martin <spencer.martin@mymail.champlain.edu>
2014-04-14 19:37:26 -0400

    Threw together a DisplaySongInfo WINDOWS FORM, OLIN chill

---
+++ MusicPlayer/MusicPlayer/Design/DisplaySongInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer.Design
+{
+    public partial class DisplaySongInfo : Form
+    {
+        public DisplaySongInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
